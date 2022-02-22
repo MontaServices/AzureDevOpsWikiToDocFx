9d046b37612be709e4cf05ffeb34f4a10b7701ab
Johan van der Slikke <jvdslikke@montapacking.nl>
2022-02-22 23:15:07 +0100

    working on enhancements

--- AzureDevOpsWikiToDocFx/AzureDevOpsWikiToDoxFx.ps1
+++ AzureDevOpsWikiToDocFx/AzureDevOpsWikiToDoxFx.ps1
@@ -23,8 +23,14 @@ $DocFxHomepageFilename = "index.md"
 $DocFxTocFilename = "toc.yml"
 $DocFxJsonFilename = "docfx.json"
 $DocFxSectionIntroductionFilename = "index.md"
-$SpecialsMarker = ":::" # For e.g. Mermaid diagrams
+$SpecialsMarker = ":::" # For Mermaid diagrams
+$SpecialsStartMarker = "[[" # For TOC and audience
+$SpecialsEndMarker = "]]"
+$TocMarker = "[[_TOC_]]"
 $AttachmentsDirName = "Attachments"
+$AudienceKeyword = "Audience"
+
+$AllMarkers = @($SpecialsMarker, $SpecialsStartMarker, $SpecialsEndMarker, $TocMarker)
 
 # Check parameters
 
@@ -47,6 +53,10 @@ if (Test-Path -Path $OutputDir) {
 if ((Test-Path -Path $TemplateDir -PathType "Container") -ne $true) {
   throw "TemplateDir does not exist"
 }
+
+#####
+# Functions
+####
 
 # Function to loop recursivly trough an Azure Devops wiki file structure to copy files to a DocFX file structure and fill a TOC file
 function Copy-Tree {
@@ -106,14 +116,17 @@ function Copy-Tree {
           $CopyItemPath = Join-Path $CopyItemPath $TocSubDirectory
         }
         $CopyItemPath = Join-Path $CopyItemPath $SubdirectoryOrderLineFileName
-        Copy-MarkdownFile -Path $CopyItemPath -Destination $CopyItemDestination -Level ($TocSubdirectories.Count + 2)
+        $ContentWritten = Copy-MarkdownFile -Path $CopyItemPath -Destination $CopyItemDestination -Level ($TocSubdirectories.Count + 2)
 
         # Check for subdirectory with the same name for subpages
-        $SubSubDirectory = Join-Path $InputDirCurrent $SubdirectoryOrderFileLine
-        if (Test-Path -Path $SubSubDirectory -PathType "Container") {
-          $NewTocSubdirectories = $TocSubdirectories.Clone()
-          $NewTocSubdirectories += $SubdirectoryOrderFileLine
-          Copy-Tree -BaseDirectory $BaseDirectory -TocFileString ([ref]$SubTocContents) -TocSubdirectories $NewTocSubdirectories
+        # Only if this page has been written
+        if ($ContentWritten) {
+          $SubSubDirectory = Join-Path $InputDirCurrent $SubdirectoryOrderFileLine
+          if (Test-Path -Path $SubSubDirectory -PathType "Container") {
+            $NewTocSubdirectories = $TocSubdirectories.Clone()
+            $NewTocSubdirectories += $SubdirectoryOrderFileLine
+            Copy-Tree -BaseDirectory $BaseDirectory -TocFileString ([ref]$SubTocContents) -TocSubdirectories $NewTocSubdirectories
+          }
         }
       }
     }
@@ -128,50 +141,124 @@ function Copy-MarkdownFile {
     [int]$Level
   )
 
-  $ThreeDotsStarted = 0;
+  $ThreeDotsStarted = 0
+  $Specials2MarkerStarted = 0
   $Silent = $false
   $ContentWritten = $false
+  $FirstLineFound = $false
+
+  # Process each line in the file
   foreach($MdLine in Get-Content -Path $Path) {
-    # Process ::: marker for mermaid and private (content to hide)
-    if ($MdLine.TrimStart().StartsWith($SpecialsMarker))
-    {
-      $RestOfLine = $MdLine.Substring(3).Trim();
-      if ($RestOfLine.Length -gt 0) {
-        if ($RestOfLine -eq "private") {
-          $Silent = $true
+    $MdLine = $MdLine.Trim()
+
+    # Loop trough each position in the string to process markers
+    $Start = 0
+    while ($Start -lt $MdLine.Length) {
+      # Check for each marker if its found on this position
+      foreach($Marker in $AllMarkers) {
+        if ($MdLine.Length -ge ($Start+($Marker.Length)) -and $MdLine.Substring($Start, $Marker.Length) -eq $Marker) {
+          # A marker is found
+
+          if ($Marker -eq $TocMarker) { # [[_TOC_]]
+            $MdLine = $MdLine.Substring(0, $Start) + $MdLine.Substring($Start + $Marker.Length).TrimStart()
+            # TODO do not write line if it became empty
+          }
+          elseif ($Marker -eq $SpecialsStartMarker) { # [[
+          }
+          elseif ($Marker -eq $SpecialsEndMarker) { # ]]
+
+          }
+          elseif ($Marker -eq $SpecialsMarker) { # :::
+
+          }
         }
-        else {
-          $MdLine = "<div class=`"$RestOfLine`">"
-        }
-        $ThreeDotsStarted += 1    
       }
-      elseif ($ThreeDotsStarted -gt 0) {
-        $MdLine = "</div>"
-        $ThreeDotsStarted -= 1
-      }
+
+      $Start += 1
     }
 
-    # Process images link path
-    $MdLine = $MdLine.Replace("](/.attachments", "](/$AttachmentsDirName")
-
-    # Make absolute links relative
+    #$NewMdLine = 
+    #$Specials2MarkerPosition = $MdLine.IndexOf($Specials2StartMarker)
+    #if ($Specials2MarkerPosition -gt -1) {
+    #  $PartBeforeSpecials2Marker = $MdLine.Substring(0, $Specials2MarkerPosition)
+    #  $RestOfLine = $MdLine.Substring($Specials2MarkerPosition)
+#
+    #  # Remove TOC
+    #  $TocMarker = "${Specials2StartMarker}_TOC_${Specials2EndMarker}"
+    #  if ($RestOfLine.StartsWith($TocMarker))
+    #  {
+    #    $RestOfLine = $RestOfLine.Substring($TocMarker.Length).TrimStart()
+    #  }
+#
+    #  # Audience
+    #  if ($RestOfLine.StartsWith($AudienceKeyword)) {
+    #    $RestOfLine = $RestOfLine.Substring($Specials2StartMarker.Length + $AudienceKeyword.Length).TrimStart().TrimStart(':').TrimStart()
+#
+    #    $RestOfLineFirstSpace = $RestOfLine.IndexOf(" ")
+    #    $AudienceSpecified
+    #    if ($RestOfLineFirstSpace -lt 0) {
+    #      $AudienceSpecified = $RestOfLine
+    #      $RestOfLine = ""
+    #    }
+    #    else {
+    #      $AudienceSpecified = $RestOfLine.Substring(0, $RestOfLineFirstSpace)
+    #      $RestOfLine = $RestOfLine.Substring($RestOfLineFirstSpace)
+    #    }
+#
+    #    $AudiencesSpecified = $AudienceSpecified -split ','
+    #  }
+#
+    #  $MdLine = $PartBeforeSpecials2Marker + $RestOfLine
+#
+    #  # If processing the marker resulted in an empty line, continue (do not write it)
+    #  if ($MdLine.Length -lt 1) {
+    #    continue
+    #  }
+    #}
+#
+    ## Skip empty lines in the beginning of the file (skip lines until first non empty line found)
+    #if ($MdLine.Length -lt 1 -and $FirstLineFound -eq $false) {
+    #  continue
+    #}
+    #$FirstLineFound = $true
+#
+    ## Process ::: marker for mermaid 
+    #if ($MdLine.StartsWith($SpecialsMarker))
+    #{
+    #  $RestOfLine = $MdLine.Substring($SpecialsMarker.Length)
+    #  if ($RestOfLine.Length -gt 0) {
+    #    if ($RestOfLine -eq "private") {
+    #      throw "Private content is not support anymore in v2, please use Audience. In file: " + $Path
+    #    }
+    #    else {
+    #      $MdLine = "<div class=`"$RestOfLine`">"
+    #    }
+    #    $ThreeDotsStarted += 1    
+    #  }
+    #  elseif ($ThreeDotsStarted -gt 0) {
+    #    $MdLine = "</div>"
+    #    $ThreeDotsStarted -= 1
+    #  }
+    #}
+#
+    ## Process images link path
+    #$MdLine = $MdLine.Replace("](/.attachments", "](/$AttachmentsDirName")
+#
+    ## Make absolute links relative
     $RelativePathPrefix = "../" * $Level
     $MdLine = $MdLine.Replace("](/", "]($RelativePathPrefix")
 
     # Write to destination file
-    if ($Silent -eq $false) {
-      Add-Content -Path $Destination -Value $MdLine
-      $ContentWritten = $true
-    }
-    elseif ($ThreeDotsStarted -lt 1) {
-      $Silent = $false
-    }
+    Add-Content -Path $Destination -Value $MdLine
+    $ContentWritten = $true
   }
 
-  if ($ContentWritten -eq $false) {
-    Add-Content -Path $Destination -Value "" # Otherwise no file will be created
-  }
+  return $ContentWritten
 }
+
+#####
+# Script starts here
+####
 
 # Search .order file
 
